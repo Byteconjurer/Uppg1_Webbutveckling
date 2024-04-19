31e18ce6be5ca2601002a313436347fea5651117
Developmentator <v.svensson.89@gmail.com>
2024-04-19 03:30:43 +0200

    added header for small displays

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    '*.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Define custom colors
+        'header-full': '#272e3a',
+        'header-small': '#28a4af',
+        'header-small-border': '#185d63',
+      },
+      screens: {
+        'sm': '0px',
+        'full': '940px'
+    },
+    },
   },
   plugins: [],
 }
